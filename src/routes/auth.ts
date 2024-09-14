3d4327b966e86eec5cd9ca7f36cb59166f7e569d
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-14 18:09:59 +0600

    auth.th page added en middleware folder

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from "express";
 import { check, validationResult } from "express-validator";
 import User from "../models/user";
 import jwt from 'jsonwebtoken';
+import verifyToken from '../middleware/auth';
 
 const router = express.Router();
 
@@ -49,4 +50,8 @@ router.post("/login", [
     }
 });
 
-export default router
+router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
+    res.status(200).send({userId: req.userId})
+});
+
+export default router;
