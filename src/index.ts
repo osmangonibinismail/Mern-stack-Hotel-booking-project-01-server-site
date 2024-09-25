087ff6f051639adec55d49c7f52ccfcf80c53a08
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-25 21:52:01 +0600

    hotels page search related info added

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import "dotenv/config";
 import mongoose from 'mongoose';
 import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import path from 'path';
-import {v2 as cloudinary} from 'cloudinary'
-import myHotelRoutes from "./routes/my-hotels"
+import {v2 as cloudinary} from 'cloudinary';
+import myHotelRoutes from "./routes/my-hotels";
+import hotelRoutes from "./routes/hotels";
 
 cloudinary.config({
     cloud_name: process.env.ClOUDINARY_CLOUD_NAME,
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes);
 app.use("/api/my-hotels", myHotelRoutes);
+app.use("/api/hotels", hotelRoutes);
 
 app.listen(7000, () => {
     console.log("server is running on localhost:7000");
