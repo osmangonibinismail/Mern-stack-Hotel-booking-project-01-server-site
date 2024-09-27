5082122b6b44d7d060d07cc059c33afdbe6791a1
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-28 01:14:55 +0600

    booking details and stripe related page added

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -21,8 +21,22 @@ export type HotelType = {
     starRating: number;
     imageUrls: string[];
     lastUpdated: Date;
+    bookings: BookingType[];
 };
 
+export type BookingType = {
+    _id: string;
+    userId: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    adultCount: number;
+    childCount: number;
+    checkIn: Date;
+    checkOut: Date;
+    totalCost: number;
+  };
+  
 export type HotelSearchResponse = {
     data: HotelType[];
     pagination: {
@@ -31,3 +45,9 @@ export type HotelSearchResponse = {
         pages: number;
     };
 };
+
+export type PaymentIntentResponse = {
+    paymentIntentId: string;
+    clientSecret: string;
+    totalCost: number;
+  };
